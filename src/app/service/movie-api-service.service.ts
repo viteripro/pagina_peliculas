d66392ffc521bcdf4610d7713de28fa5a5379a13
Luis Viteri <108193534+viteripro@users.noreply.github.com>
2023-08-28 17:30:14 -0500

    Aqui agrego los archivos correspondientes
    Estos permitirán visualizar la web

---
+++ src/app/service/movie-api-service.service.ts
@@ -0,0 +1,59 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+@Injectable({
+    providedIn: 'root'
+})
+export class MovieApiServiceService {
+    constructor(private http:HttpClient) { }
+      
+     baseurl = "https://api.themoviedb.org/3";
+     apikey = "08cc33bd5ae3a747598ce2ad84376e66";
+     
+     
+     //bannerapidata
+
+     bannerApiData():Observable<any>
+     {
+        return this.http.get(`${this.baseurl}/trending/all/week?api_key=${this.apikey}`);
+     }
+     
+
+     //trendingmovieapidata
+     trendingMovieApiData():Observable<any>
+     {
+        return this.http.get(`${this.baseurl}/trending/movie/day?api_key=${this.apikey}`);
+     }
+
+    //searchmovie
+     getSearchMovie(data:any):Observable<any>
+     {
+       console.log(data,'movie#');
+
+       return this.http.get(`${this.baseurl}/search/movie?api_key=${this.apikey}&query=${data.movieName}`);
+     }
+
+     //getmoviedetails
+     getMovieDetails(data:any):Observable<any>
+     {
+       return this.http.get(`${this.baseurl}/movie/${data}?api_key=${this.apikey}`);
+     }
+
+     // getMovieVideo
+     getMovieVideo(data:any):Observable<any>
+     {
+       return this.http.get(`${this.baseurl}/movie/${data}/videos?api_key=${this.apikey}`); 
+     }
+
+     // getMovieCast
+     getMovieCast(data:any):Observable<any>
+     {
+        return this.http.get(`${this.baseurl}/movie/${data}/credits?api_key=${this.apikey}`);
+     }
+    
+     //action
+     fetchActionMovies():Observable<any>
+     {
+        return this.http.get(`${this.baseurl}/discover/movie?api_key=${this.apikey}&width_genres=28`);
+     }
+ }
