c806462aad77d82ee852bb5cf74c8b3a7c89e919
Luis Viteri <108193534+viteripro@users.noreply.github.com>
2023-08-29 00:11:32 -0500

    Update home.component.ts
    Agregando los componentes necesarios para visualizarlo

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -12,10 +12,24 @@ export class HomeComponent implements OnInit {
 
   bannerResult:any=[];
   trendingMovieResult:any=[];
+  actionMovieResult:any=[];
+  adventureMovieResult:any=[];
+  animationMovieResult:any=[];
+  comedyMovieResult:any=[];
+  documentaryMovieResult:any=[];
+  sciencefictionMovieResult:any=[];
+  thrillerMovieResult:any=[];
 
   ngOnInit(): void {
       this.bannerData();
       this.trendingData();
+      this.actionMovie();
+      this.adventureMovie();
+      this.comedyMovie();
+      this.animationMovie();
+      this.documentaryMovie();
+      this.sciencefictionMovie();
+      this.thrillerMovie();
   }
 
   //bannerdata
@@ -33,4 +47,64 @@ export class HomeComponent implements OnInit {
       this.trendingMovieResult = result.results;
     });
    }
+
+//action
+   actionMovie()
+   {
+    this.service.fetchActionMovies().subscribe((result)=>{
+       this.actionMovieResult = result.results;
+    });
+   }
+
+    //adventure
+    adventureMovie()
+    {
+     this.service.fetchAdventureMovies().subscribe((result)=>{
+        this.adventureMovieResult = result.results;
+     });
+    }
+    
+    
+      //animation
+   animationMovie()
+   {
+    this.service.fetchAnimationMovies().subscribe((result)=>{
+       this.animationMovieResult = result.results;
+    });
+   }
+
+
+     //comedy
+     comedyMovie()
+     {
+      this.service.fetchComedyMovies().subscribe((result)=>{
+         this.comedyMovieResult = result.results;
+      });
+     }
+
+
+   //documentary
+  documentaryMovie()
+   {
+    this.service.fetchDocumentaryMovies().subscribe((result)=>{
+       this.documentaryMovieResult = result.results;
+    });
+   }    
+
+  //science-fiction
+  sciencefictionMovie()
+  {
+   this.service.fetchScienceFictionMovies().subscribe((result)=>{
+      this.sciencefictionMovieResult = result.results;
+   });
+  }
+
+   //thriller
+   thrillerMovie()
+   {
+    this.service.fetchThrillerMovies().subscribe((result)=>{
+       this.thrillerMovieResult = result.results;
+    });
+   } 
+  
 }
